ad383018898f997d24a80765d915b25af057035c
paran21 <knospe1@naver.com>
2022-02-19 22:49:28 +0900

    코드 일부 수정

--- src/main/java/com/example/marketclone/testData/SeleniumRunner.java
+++ src/main/java/com/example/marketclone/testData/SeleniumRunner.java
@@ -28,7 +28,7 @@ public class SeleniumRunner {
         options.addArguments("--start-maximized");            // 전체화면으로 실행
         options.addArguments("--disable-popup-blocking");    // 팝업 무시
         options.addArguments("--disable-default-apps");     // 기본앱 사용안함
-        //options.addArguments("headless");                   // 브라우저 실행이 내부로 전환, 눈에 안보인다.
+        options.addArguments("headless");                   // 브라우저 실행이 내부로 전환, 눈에 안보인다.
 
         // WebDriver 객체 생성
         ChromeDriver driver = new ChromeDriver(options);

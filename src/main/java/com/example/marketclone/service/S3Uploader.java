a83ee37ae4bf1d842edc8e104c982b5627b4627f
hyeji <laon1551@naver.com>
2022-02-23 17:09:56 +0900

    고혜지 - 임시 파일을 저장할 폴더를 새로 생성

--- src/main/java/com/example/marketclone/service/S3Uploader.java
+++ src/main/java/com/example/marketclone/service/S3Uploader.java
@@ -36,22 +36,28 @@ public class S3Uploader {
 
         // 1. 서버에 임시 파일 저장
 
-        // 1-1 파일 객체 생성 (경로는 현재 프로젝트가 위치하는 곳으로)
-        File tempFile = new File(System.getProperty("user.dir") + "/" + multipartFile.getOriginalFilename());
+        // 1-1 서버에 임시 파일을 저장할 폴더가 없으면 새로 생성
+        File Folder = new File(System.getProperty("user.dir")+"/tempFileFolder"); // '/'으로 적든 '\\'으로 적든 같다.
+        if (!Folder.exists()) {
+            Folder.mkdir();
+        }
 
-        // 1-2 파일 존재여부 체크, 없으면 빈 파일 생성
+        // 1-2 파일 객체 생성 (경로는 현재 프로젝트가 위치하는 곳으로)
+        File tempFile = new File(System.getProperty("user.dir") + "/tempFileFolder/" + multipartFile.getOriginalFilename());
+
+        // 1-3 파일 존재여부 체크, 없으면 빈 파일 생성
         if (!tempFile.exists()) {
             tempFile.createNewFile();
         }
 
-        // 1-3 tempFile에 내용을 작성할 수 있는 FileOutputStream 생성
-            // fileOutPutStream의 'append 디폴트 값이 false'이므로 이미 존재하는 파일이라면 기존 내용이 지워진다.
+        // 1-4 tempFile에 내용을 작성할 수 있는 FileOutputStream 생성
+            // fileOutPutStream의 'append 디폴트 값이 false'이므로 이미 내용이 존재하는 파일이라면 기존 내용이 지워진다.
         FileOutputStream fos = new FileOutputStream(tempFile);
 
-        // 1-4 가져온 multipartFile의 내용으로 채워넣기
+        // 1-5 가져온 multipartFile의 내용으로 채워넣기
         fos.write(multipartFile.getBytes());
 
-        // 1-5 다 사용한 FileOutputStream 닫기
+        // 1-6 다 사용한 FileOutputStream 닫기
         fos.close();
 
 
@@ -65,11 +71,8 @@ public class S3Uploader {
         String uploadImageUrl = amazonS3Client.getUrl(bucket, fileName).toString();
 
         // 3. 임시 파일 삭제
-
-        if (tempFile.delete()) {
-            log.info("File delete success");
-        } else {
-            log.info("File delete fail");
+        if (tempFile.exists()) {
+            tempFile.delete();
         }
 
         // 4. S3에 저장된 파일 이름과 주소 반환
